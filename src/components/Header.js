da61fae6a5ecd13fa00ca905a966f82bb9e0066a
pisces7diana <email7diana@gmail.com>
2024-01-29 15:57:57 -0800

    Header.js done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 const Header = (props) => {
     return <div className="Header">
         <h1>Welcome to Diana Hudson's General Assembly Portfolios</h1>
-        
+
+        <nav>
+        <a href="https://github.com/pisces7diana"><button>GitHub</button></a>
+        <a href="https://www.linkedin.com/in/diana-dang-hudson/"><button>LinkedIn</button></a>
+        </nav>
+
     </div>
 }
 
