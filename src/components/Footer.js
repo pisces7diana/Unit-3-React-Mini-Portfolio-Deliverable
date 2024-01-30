a5471a9b11aecc76f656322edc685eabb4f106ef
pisces7diana <email7diana@gmail.com>
2024-01-29 18:24:23 -0800

    Footer.js done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,15 @@
 const Footer = (props) => {
     return <div className="Footer">
-        <h1>This is the Footer</h1>
+
+        <div className="Diana-Hudson">
+            <h1>Diana Dang Hudson</h1>
+            <h2>Chula Vista, CA 91910</h2>
+            <p>&hearts; to hear from you for your Web Development needs:</p>
+            <span><b>Phone: +1 (619) 598-8772</b> | <b>Email: email7diana@gmail.com</b></span>
+            <h3>Social Media:<a href="www.facebook.com"><button>Facebook</button></a><a href="www.instagram.com"><button>Instagram</button></a></h3>
+            <h3>Terms of Service</h3>
+        </div>
+
     </div>
 }
 
